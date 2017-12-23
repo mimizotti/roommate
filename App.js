ebbad716e359546f43b93601099741f27190e54d
mimilettd <mimilettd@gmail.com>
2017-12-23 11:16:49 -0700

    create login and password input fields and submit button

--- App.js
+++ App.js
@@ -15,13 +15,25 @@ import {
   Image,
   TextInput
 } from 'react-native';
-import Logo from './src/components/Logo'
+import Logo from './src/components/welcome/Logo'
+import Login from './src/components/welcome/Login'
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <Logo />
+      <View style={styles.container}>
+        <Logo />
+        <Login />
+      </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //justifyContent: 'center',
+    //alignItems: 'center',
+    backgroundColor: '#0bb791',
+  },
+});
---
+++ src/components/welcome/Login.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {
+    ScrollView,
+    Text,
+    TextInput,
+    View,
+    Button,
+    StyleSheet,
+    Image
+} from 'react-native';
+import LoginForm from './LoginForm'
+
+export default class Login extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.loginContainer}>
+                    <Image resizeMode="contain" style={styles.logo} />
+         </View>
+
+            <View style={styles.formContainer}>
+                   <LoginForm />
+            </View>
+       </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+  },
+  loginContainer:{
+      alignItems: 'center',
+      flexGrow: 1,
+      justifyContent: 'center'
+  }
+  //logo: {
+      //position: 'absolute',
+      //width: 300,
+      //height: 100
+  //}
+})
