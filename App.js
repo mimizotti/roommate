974992010440083efea1a087097afa1d5960163c
mimilettd <mimilettd@gmail.com>
2018-01-03 12:26:09 -0700

    Initialize navigator

--- App.js
+++ App.js
@@ -13,17 +13,54 @@ import {
   Text,
   View,
   Image,
-  TextInput
+  TextInput,
+  Button
 } from 'react-native';
 import Logo from './src/components/welcome/Logo'
 import Login from './src/components/welcome/Login'
+import { StackNavigator } from 'react-navigation';
 
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Button
+          onPress={() => navigate('Chat')}
+          title="Chat with Lucy"
+        />
+      </View>
+    );
+  }
+}
+
+class ChatScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Chat with Lucy',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Chat with Lucy</Text>
+      </View>
+    );
+  }
+}
+
+export const SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen },
+  Chat: { screen: ChatScreen },
+});
 export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
         <Logo />
         <Login />
+        <SimpleApp />
       </View>
     );
   }
